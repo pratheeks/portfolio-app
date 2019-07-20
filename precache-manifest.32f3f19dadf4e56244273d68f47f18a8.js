c55c835ffc89027e5ab5af62badc1a0d9b0d8ed9
prastik <pratheekseraje@gmail.com>
2019-07-20 17:03:11 +0530

    Updates

---
+++ precache-manifest.32f3f19dadf4e56244273d68f47f18a8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9697899103c36852d388292e90e6ed94",
+    "url": "/portfolio-app/index.html"
+  },
+  {
+    "revision": "4b0228a9829c60716e86",
+    "url": "/portfolio-app/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/portfolio-app/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "4b0228a9829c60716e86",
+    "url": "/portfolio-app/static/js/main.e169d157.chunk.js"
+  },
+  {
+    "revision": "44340542c48e10c20cd6",
+    "url": "/portfolio-app/static/js/runtime~main.cb97d77a.js"
+  }
+]);
